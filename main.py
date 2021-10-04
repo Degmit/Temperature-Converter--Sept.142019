461576450099e2c84b08564fbca6590079de9c4a
Mitzy Degante <50189260+Degmit@users.noreply.github.com>
2021-10-04 01:24:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+celsiusTemperature = float(input("Enter a temperature in celsius: "))
+farenheitTemperature = ((9/5) * celsiusTemperature) + 32
+
+print( str( celsiusTemperature ) + " degrees celsius \
+in Farenheit is: " +  str( farenheitTemperature ))
